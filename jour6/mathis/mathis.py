dffe6b847c068bfbef3eb644dde4796a17fa9b85
Mathis Benjamin Manuel Randl <mathis.randl@epfl.ch>
2023-12-06 18:04:03 +0100

    alternative to sol6 by equations

--- jour6/mathis/mathis.py
+++ jour6/mathis/mathis.py
@@ -1,7 +1,14 @@
 from functools import reduce
+import math
 
 def is_winning_run(wait_time, total_time, current_record):
     return (total_time - wait_time) * wait_time > current_record
+
+def solve_analytical(total_time, current_record):
+    delta = (total_time ** 2) - (4 * current_record)
+    root1 = math.ceil((total_time - math.sqrt(delta)) / 2)
+    root2 = math.floor((total_time + math.sqrt(delta)) / 2)
+    return int(root2 - root1 + 1)
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
@@ -11,18 +18,11 @@ with open("input.txt", "r") as f:
     #part 1
     summs = []
     for (time_of_run, record) in zip(times, distances):
-        summ = 0
-        for i in range(time_of_run):
-            if is_winning_run(i, time_of_run, record):
-                summ += 1
-        summs.append(summ)
+        summs.append(solve_analytical(time_of_run, record))
     print("solution 1 :", reduce(lambda x, y: x*y, summs))
 
     #part 2
     time = int(''.join([x for x in lines[0] if x.isdigit()]))
     dist = int(''.join([x for x in lines[1] if x.isdigit()]))
-    summ = 0
-    for i in range(time):
-        if is_winning_run(i, time, dist):
-            summ += 1
-    print("solution 2 ;", summ)
+    print("solution 2 ", solve_analytical(time, dist))
+
