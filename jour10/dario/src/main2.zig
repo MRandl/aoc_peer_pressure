2c248168834882f89bdc3c156b26ec461a160058
Anon <anon@anon.anon>
2023-12-16 21:28:05 +0100

    jour 10 dario part 1

--- jour10/dario/src/main2.zig
+++ jour10/dario/src/main2.zig
@@ -34,13 +34,36 @@ pub fn main() !void {
         }
     }
 
-    var distance : u32 = 1;
+    var area : i32 = 0;
 
     //unwrap the first iteration of the loop
     curr_point[0] -= 1;
     var tube = input[curr_point[0]][curr_point[1]];
     while (tube != 'S') {
-        defer distance += 1;
+        // We're in the situation:
+        // Going UP from S.
+        //
+        // ........
+        // ...F-7..
+        // ..FJ.L7.
+        // ..|..FJ.
+        // .FS..|..
+        // .L7.FJ..
+        // ..L-J...
+        // ........
+        //
+        // By trial and error, we choose
+        // that going UP means sign = -1,
+        // going DOWN means sign = +1
+        // in order to get a positive result.
+        var sign : i32 = 0;
+        // Since the interior does not include the tubes
+        // need to subtract 1 at some point.
+        // we can subtract every time sign is +1
+        // or everytime sign is -1.
+        // Choosing the latter, the correction term is:
+        // correction term = MIN(0, sign)
+        defer area += sign * @as(i32, @intCast(curr_point[1])) + @min(0, sign);
         defer tube = input[curr_point[0]][curr_point[1]];
         switch (tube) {
             '7' => {
@@ -48,18 +71,24 @@ pub fn main() !void {
                     curr_dir = DOWN;
                 } else if (curr_dir == UP) {
                     curr_dir = LEFT;
+                    sign = -1;
                 } else unreachable;
             },
-            'J' => {
+            'J','S' => {
                 if (curr_dir == DOWN) {
                     curr_dir = LEFT;
                 } else if (curr_dir == RIGHT) {
                     curr_dir = UP;
+                    sign = -1;
                 } else unreachable;
+                if (tube == 'S') {
+                    break;
+                }
             },
             'L' => {
                 if (curr_dir == DOWN) {
                     curr_dir = RIGHT;
+                    sign = 1;
                 } else if (curr_dir == LEFT) {
                     curr_dir = UP;
                 } else unreachable;
@@ -67,13 +96,19 @@ pub fn main() !void {
             'F' => {
                 if (curr_dir == LEFT) {
                     curr_dir = DOWN;
+                    sign = 1;
                 } else if (curr_dir == UP) {
                     curr_dir = RIGHT;
                 } else unreachable;
             },
             '-' => {},
-            '|' => {},
-            'S' => break,
+            '|' => {
+                if (curr_dir == UP) {
+                    sign = -1;
+                } else if (curr_dir == DOWN) {
+                    sign = 1;
+                } else unreachable;
+            },
             else => unreachable // we are in the main loop (because I know the input)
         }
         switch (curr_dir) {
@@ -85,6 +120,6 @@ pub fn main() !void {
         }
     }
 
-    std.log.debug("Distance/2 = {}", .{distance/2});
+    std.log.debug("Area = {}", .{area});
 
 }
